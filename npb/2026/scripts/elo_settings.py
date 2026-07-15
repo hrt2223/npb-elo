3c2ed13229062d11d97b1e7f2da1231c9fc83d60
hiroto <hirofuku161024@gmail.com>
2026-07-15 20:25:02 +0900

    chore: recalculate NPB Elo outputs

--- npb/2026/scripts/elo_settings.py
+++ npb/2026/scripts/elo_settings.py
@@ -8,7 +8,7 @@ K_FACTOR = 24.0
 
 # Expected score = 1 / (1 + LOGISTIC_BASE ** (rating_difference / LOGISTIC_SCALE))
 LOGISTIC_BASE = 10.0
-LOGISTIC_SCALE = 1850.0
+LOGISTIC_SCALE = 2050.0
 
 # Rating points added to the home team when calculating expected score.
 HOME_ADVANTAGE = 28.0
